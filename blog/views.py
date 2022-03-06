9b1e7af61d09b5b7e96243ee9fd790268bca729b
kabin basnet <kabinbasnet22@gmail.com>
2022-03-06 17:13:23 +0545

    about.html and home.html code written of templates/blog folder and such html pages render code written on views of blog app.

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
