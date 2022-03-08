cd1a5fe9710750e0247c91f5e176f9a9b2962507
kabin basnet <kabinbasnet22@gmail.com>
2022-03-08 09:39:25 +0545

    Date Format Changed in blog/home,html and dummy data comment were done.

--- blog/views.py
+++ blog/views.py
@@ -4,20 +4,20 @@ from .models import Post
 
 # ::::::::Creating dummy data in list form:::::::::::::::::::::::::::
 # Dummy data ==>> being information that doesnot contain any useful data, but serves to reserve space.
-posts = [
-    {
-        'author': 'Kabin Basnet',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'March 1, 2022'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'March 2, 2022'
-    }
-]
+# posts = [
+#     {
+#         'author': 'Kabin Basnet',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_posted': 'March 1, 2022'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_posted': 'March 2, 2022'
+#     }
+# ]
 
 
 def home(request):
