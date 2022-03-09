b7429e54614eb92d8000a1ad2f1330643764840b
kabin basnet <kabinbasnet22@gmail.com>
2022-03-09 17:52:13 +0545

    responsive with message code written on users/register.py and to so such messages in required page we written some design code on blog/templates/blog/base.html.

--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
 
 def register(request):
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserCreationForm()
+        return render(request, 'users/register.html', {'form': form})
