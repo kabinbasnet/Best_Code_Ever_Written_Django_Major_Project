ba090a45fa72bcca47894b5c9fa8bbfcebf88a4a
kabin basnet <kabinbasnet22@gmail.com>
2022-03-15 14:50:48 +0545

    reverse url defined inside the models.py and LoginRequiredMixeddefined in blog/views.py same as Login_Required decorator.

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.forms import modelformset_factory
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 # Create your models here.
 
 
@@ -15,3 +15,6 @@ class Post(models.Model):
     def __str__(self):
         # Creating dunder method where dunder ==>> double underscore.
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
