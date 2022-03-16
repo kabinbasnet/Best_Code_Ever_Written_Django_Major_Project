c8de4fc15aa90c6718734cabd01a607ae5df95b4
kabin basnet <kabinbasnet22@gmail.com>
2022-03-16 15:45:26 +0545

    Pagination code defined on home.html + views.py and New json file added name is posts.json

--- blog/views.py
+++ blog/views.py
@@ -45,8 +45,12 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
 
     context_object_name = 'posts'
+
     # -date_posted ==> gives recent to oldest date time views
     ordering = ['-date_posted']
+
+    # ::::::::::paginate_by means give pagination(a process used to divide a large data into smaller discrete pages) after 2 posts::::::::::::::
+    paginate_by = 2
 
 
 class PostDetailView(DetailView):
